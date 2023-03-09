996fa832bc027efaecd8b3af3459bea899ef36c0
Newasw <62440057+Newasw@users.noreply.github.com>
2023-03-09 21:37:30 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 #include <iostream>
 #include <string.h>
 #include <list>
@@ -7,7 +6,7 @@
 #include"polynomial.h"
 using namespace std;
 
-int Take_line(char z, string s) {
+int Take_line(char z, string s) {  // "вырезка " части строки до конкретного символа "z"
 	unsigned int i = 0;
 	char cur;
 	cur = s[i];
@@ -17,7 +16,7 @@ int Take_line(char z, string s) {
 	return i;
 }
 
-const char* Tochar(string s) {
+const char* Tochar(string s) { // переводит строку в const char *
 	char* c[sizeof(s)];
 	int i = -1;
 	while (i < (sizeof(s) - 1)) {
@@ -27,7 +26,7 @@ const char* Tochar(string s) {
 	return b;
 }
 
-string ToString(char* c) {
+string ToString(char* c) { // переводит char * в строку
 	char cur;
 	string res;
 	int i = -1;
@@ -39,7 +38,7 @@ string ToString(char* c) {
 	return res;
 }
 
-struct Node
+struct Node // структура которая отвечает за строки таблицы
 {
 	unsigned int id;
 	string name;
@@ -56,10 +55,8 @@ public:
 		data = ToString(strncpy(c,&s[Take_line(' ', s) + 2], sizeof(s) - Take_line(' ', s) - 1));
 	}*/
 };
-class Table //абстрактный класс
+class Abstract //абстрактный класс таблицы, от которого наследуются остальные таблицы
 {
-public:
-
 	virtual void Put(Node val) = 0; // добавить новый элемент в таблицу
 	virtual void Del(string name) = 0; // удалить элемент по имени
 	virtual void Del(int id) = 0; // удалить элемент по id
@@ -70,287 +67,3 @@ public:
 	virtual void Print() = 0;// печать таблицы 
 };
 
-class Lmas : public Table
-{
-private:
-	Node* row;
-	int marker; // отмечает индекс последнего записанного элемента
-public:
-	Lmas() {
-		row = new Node[1];
-		marker = -1;
-	}
-	void Put(Node val) {
-		if (marker == -1) {
-			marker = 0;
-			row[marker] = val;
-		}
-		else {
-			if ((marker == sizeof(row) / 4) || marker == 0) {
-				Node* tmp = new Node[sizeof(row) *2];
-				for (int i = 0; i < (sizeof(row) / 4); i++) {
-					tmp[i] = row[i];
-				}
-				delete[]row;
-
-				row = tmp;
-			}
-			row[++marker] = val;
-		}
-	}
-	void Del(string name) {
-		int i = Search(name);
-		if (i == marker) {
-			marker--;
-		}
-		else {
-			while (i < marker) {
-				row[i] = row[i + 1];
-				i++;
-			}
-			marker--;
-		}
-	}
-	void Del(int id) {
-		int i = Search(id);
-		if (i == marker) {
-			marker--;
-		}
-		else{
-		while (i < marker) {
-			row[i] = row[i + 1];
-			i++;
-		}
-		marker--;
-		}
-	}
-	int Search(string name) {
-		int i = 0;
-		try
-		{
-			while (i <= marker) {
-				if (name == row[i].name) {
-					return i;
-				}
-				i++;
-			}
-			throw"ERROR";
-		}
-		catch (const std::exception&)
-		{
-			cout << "name not found" << endl;
-		}
-
-	}
-	int Search(int id) {
-		int i = 0;
-		try
-		{
-			while (i <= marker) {
-				if (id == row[i].id) {
-					return i;
-				}
-				i++;
-			}
-			throw"ERROR";
-		}
-		catch (const std::exception&)
-		{
-			cout << "id not found" << endl;
-		}
-
-	}
-	Polynome Take_elem(string name) {
-		return row[Search(name)].data;
-	}
-	Polynome Take_elem(int id) {
-		return row[Search(id)].data;
-	}
-	void Print() {
-		int i = 0;
-		cout << "Line mas table" << endl;
-		while (i <= marker) {
-			cout << "------------------------------------" << endl;
-			cout << row[i].id << " | " << row[i].name << " | ";
-			row[i].data.print();
-			cout << endl;
-			i++;
-		}
-	}
-	~Lmas() {
-		delete[]row;
-		row = nullptr;
-	}
-
-
-};
-
-class List : public Table
-{
-private:
-	list <Node> rows;
-public:
-	List() {
-	};
-	void Put(Node val) {
-		rows.push_back(val);
-	}
-	int Search(string name) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).name == name) {
-					return i;
-				}
-				i++;
-			}
-			throw"ERROR";
-
-		}
-		catch (const std::exception&)
-		{
-			cout << "name not found" << endl;
-		}
-
-
-	}
-	int Search(int id) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).id == id) {
-					return i;
-				}
-				i++;
-			}
-			throw"ERROR";
-
-		}
-		catch (const std::exception&)
-		{
-			cout << "id not found" << endl;
-		}
-
-
-	}
-
-	Polynome Take_elem(string name) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).name == name) {
-					return (*Iter).data;
-				}
-				i++;
-			}
-			throw"ERROR";
-		}
-		catch (const std::exception&)
-		{
-			cout << "name not found" << endl;
-		}
-
-	}
-	Polynome Take_elem(int id) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).id == id) {
-					return (*Iter).data;
-				}
-				i++;
-			}
-			throw"ERROR";
-		}
-		catch (const std::exception&)
-		{
-			cout << "id not found" << endl;
-		}
-
-	}
-	void Del(string name) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		bool k = false;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).name == name) {
-					rows.erase(Iter);
-					k = true;
-					break;
-				}
-				i++;
-			}
-			if (k == false) {
-				throw"ERROR";
-			}
-		}
-		catch (const std::exception&)
-		{
-			cout << "id not found" << endl;
-		}
-	}
-	void Del(int id) {
-		list <Node>::iterator Iter;
-		int i = 0;
-		bool k = false;
-		try
-		{
-			for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-				if ((*Iter).id == id) {
-					rows.erase(Iter);
-					k = true;
-					break;
-				}
-				i++;
-			}
-			if(k == false){
-			throw"ERROR";
-			}
-		}
-		catch (const std::exception&)
-		{
-			cout << "id not found" << endl;
-		}
-	}
-	void Print() {
-		list <Node>::iterator Iter;
-		for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
-			cout << "------------------------------------" << endl;
-			cout << (*Iter).id << " | " << (*Iter).name << " | ";
-			(*Iter).data.print();
-			cout << endl;
-		}
-	}
-	~List() {
-		rows.clear();
-	};
-
-
-
-};
-
-class Cmas
-{
-public:
-	Cmas() {}
-	void Put(string val) {}
-	void Del(string name) {}
-	void Del(int id) {}
-	int Search(string name) {}
-	string Take_elem(string name) {}
-	void Print() {}
-	~Cmas() {}
-
-private:
-
-};
