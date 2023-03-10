7dc6722e8d38bcc8c7ee6d41bbaa93d4346eeb61
Newasw <62440057+Newasw@users.noreply.github.com>
2023-03-10 21:29:54 +0300

    Update Table.h

--- Table.h
+++ Table.h
@@ -6,37 +6,6 @@
 #include"polynomial.h"
 using namespace std;
 
-int Take_line(char z, string s) {  // "вырезка " части строки до конкретного символа "z"
-	unsigned int i = 0;
-	char cur;
-	cur = s[i];
-	while (cur != z || i != sizeof(s)) {
-		cur = s[i++];
-	}
-	return i;
-}
-
-const char* Tochar(string s) { // переводит строку в const char *
-	char* c[sizeof(s)];
-	int i = -1;
-	while (i < (sizeof(s) - 1)) {
-		c[++i] = &s[i];
-	}
-	const char* b = *c;
-	return b;
-}
-
-string ToString(char* c) { // переводит char * в строку
-	char cur;
-	string res;
-	int i = -1;
-	while (i < (sizeof(c) - 1))
-	{
-		cur = c[++i];
-		res += cur;
-	}
-	return res;
-}
 
 struct Node // структура которая отвечает за строки таблицы
 {
@@ -48,12 +17,6 @@ public:
 		id = rand();
 		name = " ";
 	}
-	/*Node(string s) {
-		id = 0;
-		char c[100] ;
-		name = ToString(strncpy(c,Tochar(s), Take_line(' ', s)));
-		data = ToString(strncpy(c,&s[Take_line(' ', s) + 2], sizeof(s) - Take_line(' ', s) - 1));
-	}*/
 };
 class Abstract //абстрактный класс таблицы, от которого наследуются остальные таблицы
 {
