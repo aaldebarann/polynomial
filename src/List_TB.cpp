9ee20ff359f8c4dd6c8c8ad08bfbf3b34ac9d726
Newasw <62440057+Newasw@users.noreply.github.com>
2023-03-17 22:46:06 +0300

    Add files via upload

---
+++ src/List_TB.cpp
@@ -0,0 +1,77 @@
+#include "List_TB.h"
+List_TB::List_TB() {
+}
+void List_TB::Insert(Node val) {
+	rows.push_back(val);
+}
+int List_TB::Search(string name) {
+	list <Node>::iterator Iter;
+	int i = 0;
+
+		for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
+			if ((*Iter).name == name) {
+				return i;
+			}
+			i++;
+		}
+
+
+
+
+}
+
+
+Polynome List_TB::Take_elem(string name) {
+	list <Node>::iterator Iter;
+	int i = 0;
+	try
+	{
+		for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
+			if ((*Iter).name == name) {
+				return (*Iter).data;
+			}
+			i++;
+		}
+		throw invalid_argument("ERROR");
+	}
+	catch (const std::exception&)
+	{
+		cout << "name not found" << endl;
+	}
+
+};
+void List_TB::Del(string name) {
+	list <Node>::iterator Iter;
+	int i = 0;
+	bool k = false;
+	try
+	{
+		for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
+			if ((*Iter).name == name) {
+				rows.erase(Iter);
+				k = true;
+				break;
+			}
+			i++;
+		}
+		if (k == false) {
+			throw invalid_argument("ERROR");
+		}
+	}
+	catch (const std::exception&)
+	{
+		cout << "id not found" << endl;
+	}
+};
+void List_TB::Print() {
+	list <Node>::iterator Iter;
+	for (Iter = rows.begin(); Iter != rows.end(); Iter++) {
+		cout << "------------------------------------" << endl;
+		cout <<(*Iter).name << " | ";
+		(*Iter).data.print();
+		cout << endl;
+	}
+};
+List_TB ::~List_TB() {
+	rows.clear();
+};
