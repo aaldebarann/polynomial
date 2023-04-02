f0daf364ba2d8d36e6d1196332fb51059451f363
Newasw <62440057+Newasw@users.noreply.github.com>
2023-04-02 20:55:56 +0300

    Add files via upload
    Интерфейс, C#

---
+++ include/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Button = new WinFormsApp1.My_Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(200, 200);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 35);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Введите выражение";
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.Color.Tomato;
+            this.Button.BackColorAdditional = System.Drawing.Color.Gray;
+            this.Button.BackColorGradientEnabled = false;
+            this.Button.BackColorGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Horizontal;
+            this.Button.BorderColor = System.Drawing.Color.Tomato;
+            this.Button.BorderColorEnabled = false;
+            this.Button.BorderColorOnHover = System.Drawing.Color.Tomato;
+            this.Button.BorderColorOnHoverEnabled = false;
+            this.Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Button.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Button.ForeColor = System.Drawing.Color.White;
+            this.Button.Location = new System.Drawing.Point(325, 279);
+            this.Button.Name = "Button";
+            this.Button.RippleColor = System.Drawing.Color.Yellow;
+            this.Button.RoundingEnable = true;
+            this.Button.Size = new System.Drawing.Size(144, 44);
+            this.Button.TabIndex = 7;
+            this.Button.Text = "Нажми";
+            this.Button.TextHover = null;
+            this.Button.UseDownPressEffectOnClick = false;
+            this.Button.UseRippleEffect = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Волшебный калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+ 
+        private System.Windows.Forms.TextBox textBox1;
+        private My_Button Button;
+    }
+}
+
